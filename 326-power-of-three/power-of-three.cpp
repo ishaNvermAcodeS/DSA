d01cbbedfd9191dd52f8becc944bc7a054bb5f00
Ishan Verma <iamvermaishan@gmail.com>
2026-09-08 19:11:34 +0530

    Time: 7 ms (22.22%) | Memory: 8.8 MB (97.42%) - LeetSync

---
+++ 326-power-of-three/power-of-three.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n <= 0){
+            return false;
+        }
+        long long x = 1;
+        while(x <= n){
+            if(x == n){
+                return true;
+            }
+            else{
+                x = x * 3;
+            }
+        }
+        return false;
+    }
+};
